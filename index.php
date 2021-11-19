f04957e981b2ec48a516792354e1e41f7d146f85
yurike27 <yuriketriyani2020@gmail.com>
2021-11-19 07:58:28 +0700

    versi pertama saya

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+
+          <!-- ini adalah pembuka navbar -->
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+              <div class="container-fluid">
+                  <a class="navbar-brand" href="#">yurike</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Hoby</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Cita Cita</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- ini adalah penutup navbar -->
+            <!-- content 1 -->
+            <div class="container">
+         <div class="row">
+    <div class="col-4">
+    <div class="card mx-auto" style="width: 18rem;">
+  <img src="assets/img/yurike.jpeg "class="card-img-top" style="width: 9rem;">
+  <div class="card-body">
+    <p class="card-text">yurike</p>
+  </div>
+</div>
+    </div>
+    <div class="col-8">
+      <h1>Selamat datang di website saya</h1>
+      <p>Perkenalkan naama saya yurike dari kelas XI Rpl 
+        kalian bisa panggil saya senyamannya kalian,saya sangat suka menggambar karna dari pas setelah lombah
+        saya jdi tertarik dengan gambar saya sangat suka parkjongseong dan saya sangat ingin
+        kekorea dan cita cita saya ingin menjadi arsitek tapi saya belum yakin ingin masuk kuliah dengan
+        jurusan itu semogaa saya berhasil yaa terimakasi </p>
+    </div>
+  </div>
+  <!-- content 1 -->
+</div>
+        </div>
+        <!-- akhir pembungkus -->
+
+    <!-- Link Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
